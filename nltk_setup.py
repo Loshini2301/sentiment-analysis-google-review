418beedc35388b835d20572c12f67bd689055eff
Loshini2301 <150007305+Loshini2301@users.noreply.github.com>
2025-12-23 20:25:19 +0530

    Update nltk_setup.py

--- nltk_setup.py
+++ nltk_setup.py
@@ -1,2 +1,10 @@
 import nltk
-nltk.download("stopwords", quiet=True)
+import os
+
+nltk_data_dir = "/opt/render/nltk_data"
+os.makedirs(nltk_data_dir, exist_ok=True)
+
+nltk.data.path.append(nltk_data_dir)
+nltk.download("stopwords", download_dir=nltk_data_dir, quiet=True)
+
+
